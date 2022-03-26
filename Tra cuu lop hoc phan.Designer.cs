16e54948c345b69cd92377b4a1555eb50a6a97a3
Huỳnh Hữu Phúc <92789962+31191027399@users.noreply.github.com>
2022-03-27 02:16:21 +0700

    initial commit

---
+++ Tra cuu lop hoc phan.Designer.cs
@@ -0,0 +1,329 @@
+﻿namespace GIAO_DIEN
+{
+    partial class Tra_cuu_lop_hoc_phan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.traCứuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.comboBox[5] = new System.Windows.Forms.ComboBox();
+            this.comboBox[4] = new System.Windows.Forms.ComboBox();
+            this.comboBox[3] = new System.Windows.Forms.ComboBox();
+            this.comboBox[2] = new System.Windows.Forms.ComboBox();
+            this.comboBox[1] = new System.Windows.Forms.ComboBox();
+            this.comboBox[0] = new System.Windows.Forms.ComboBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox[6] = new System.Windows.Forms.ComboBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem,
+            this.traCứuToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(437, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(60, 24);
+            this.menuToolStripMenuItem.Text = "Menu";
+            // 
+            // traCứuToolStripMenuItem
+            // 
+            this.traCứuToolStripMenuItem.Name = "traCứuToolStripMenuItem";
+            this.traCứuToolStripMenuItem.Size = new System.Drawing.Size(71, 24);
+            this.traCứuToolStripMenuItem.Text = "Tra cứu";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(0, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(343, 38);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Tra cứu các lớp học phần";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // comboBox6
+            // 
+            this.comboBox[5].FormattingEnabled = true;
+            this.comboBox[5].Location = new System.Drawing.Point(154, 346);
+            this.comboBox[5].Name = "comboBox6";
+            this.comboBox[5].Size = new System.Drawing.Size(189, 28);
+            this.comboBox[5].TabIndex = 29;
+            // 
+            // comboBox5
+            // 
+            this.comboBox[4].FormattingEnabled = true;
+            this.comboBox[4].Location = new System.Drawing.Point(154, 309);
+            this.comboBox[4].Name = "comboBox5";
+            this.comboBox[4].Size = new System.Drawing.Size(189, 28);
+            this.comboBox[4].TabIndex = 28;
+            // 
+            // comboBox4
+            // 
+            this.comboBox[3].FormattingEnabled = true;
+            this.comboBox[3].Location = new System.Drawing.Point(154, 271);
+            this.comboBox[3].Name = "comboBox4";
+            this.comboBox[3].Size = new System.Drawing.Size(189, 28);
+            this.comboBox[3].TabIndex = 27;
+            // 
+            // comboBox3
+            // 
+            this.comboBox[2].FormattingEnabled = true;
+            this.comboBox[2].Location = new System.Drawing.Point(154, 235);
+            this.comboBox[2].Name = "comboBox3";
+            this.comboBox[2].Size = new System.Drawing.Size(189, 28);
+            this.comboBox[2].TabIndex = 26;
+            // 
+            // comboBox2
+            // 
+            this.comboBox[1].FormattingEnabled = true;
+            this.comboBox[1].Location = new System.Drawing.Point(154, 197);
+            this.comboBox[1].Name = "comboBox2";
+            this.comboBox[1].Size = new System.Drawing.Size(189, 28);
+            this.comboBox[1].TabIndex = 25;
+            // 
+            // comboBox1
+            // 
+            this.comboBox[0].FormattingEnabled = true;
+            this.comboBox[0].Location = new System.Drawing.Point(154, 157);
+            this.comboBox[0].Name = "comboBox1";
+            this.comboBox[0].Size = new System.Drawing.Size(189, 28);
+            this.comboBox[0].TabIndex = 24;
+         
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(72, 354);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(37, 20);
+            this.label9.TabIndex = 23;
+            this.label9.Text = "Lớp:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(17, 317);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(100, 20);
+            this.label7.TabIndex = 22;
+            this.label7.Text = "Năm vào học:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(19, 279);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(98, 20);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "Chương trình:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(62, 243);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 20);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Ngành:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(72, 205);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 20);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Khoa:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 20);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Hệ đào tạo:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 20);
+            this.label2.TabIndex = 30;
+            this.label2.Text = "Mã học phần:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(72, 88);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(50, 20);
+            this.label8.TabIndex = 31;
+            this.label8.Text = "MSSV:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(154, 81);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(189, 27);
+            this.textBox1.TabIndex = 32;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(154, 120);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(189, 27);
+            this.textBox2.TabIndex = 33;
+            // 
+            // comboBox7
+            // 
+            this.comboBox[6].FormattingEnabled = true;
+            this.comboBox[6].Location = new System.Drawing.Point(154, 385);
+            this.comboBox[6].Name = "comboBox7";
+            this.comboBox[6].Size = new System.Drawing.Size(189, 28);
+            this.comboBox[6].TabIndex = 35;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(72, 393);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(70, 20);
+            this.label10.TabIndex = 34;
+            this.label10.Text = "Môn học:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(175, 440);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 29);
+            this.button1.TabIndex = 36;
+            this.button1.Text = "Tra cứu";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Tra_cuu_lop_hoc_phan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(437, 481);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox[6]);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox[5]);
+            this.Controls.Add(this.comboBox[4]);
+            this.Controls.Add(this.comboBox[3]);
+            this.Controls.Add(this.comboBox[2]);
+            this.Controls.Add(this.comboBox[1]);
+            this.Controls.Add(this.comboBox[0]);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Tra_cuu_lop_hoc_phan";
+            this.Text = "Tra cuu lop hoc phan";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem menuToolStripMenuItem;
+        private ToolStripMenuItem traCứuToolStripMenuItem;
+        private Label label1;
+        private ContextMenuStrip contextMenuStrip1;
+
+        private ComboBox[] comboBox;
+        ////private ComboBox comboBox6;
+        ////private ComboBox comboBox5;
+        ////private ComboBox comboBox4;
+        //private ComboBox comboBox3;
+        //private ComboBox comboBox2;
+        //private ComboBox comboBox1;
+        private Label label9;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label8;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        //private ComboBox comboBox7;
+        private Label label10;
+        private Button button1;
+
+       
+    }
+
+    
+}
---
+++ Tra cuu lop hoc phan.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace GIAO_DIEN
+{
+    public partial class Tra_cuu_lop_hoc_phan : Form
+    {
+        string Sqlstring = @"Data Source=DESKTOP-J79GQJ1\SQLEXPRESS;Initial Catalog=QLSVtrang;Integrated Security=True";
+        SqlConnection SqlCon = null;
+
+
+        public Tra_cuu_lop_hoc_phan()
+        {
+            comboBox = new ComboBox[7];
+            InitializeComponent();
+
+            
+
+            String suffer = @"USE QLSVtrang; SELECT DISTINCT @X FROM (SELECT E.bachoc,E.tenkh,E.tenng,E.tenct,E.namvao,E.malophoc,E.tenmon FROM (SELECT D.*,MH.tenmon FROM MONHOC AS MH 
+RIGHT JOIN (SELECT C.*,LHP.malophp,LHP.mamon FROM  LOPHOCPHAN AS LHP RIGHT JOIN (SELECT B.*, LH.malophoc,LH.namvao  FROM LOPHOC AS LH RIGHT JOIN (SELECT A.*,NG.tenng
+FROM NGANH AS NG RIGHT JOIN (SELECT CT.*,K.tenkh FROM KHOA AS K RIGHT JOIN CHUONGTRINH AS CT ON CT.makh=K.makh) AS A ON A.mang=NG.mang) AS B on B.mact=LH.mact) AS C
+ON C.malophoc=LHP.malophoc) AS D ON D.mamon=MH.mamon) AS E) as I WHERE @X IS NOT NULL and 1=1 and 2=2 and 3=3 and 4=4 and 5=5 and 6=6 and 7=7";
+            //them DS HEDAOTAO
+            if (SqlCon == null)
+            {
+                SqlCon = new SqlConnection(Sqlstring);
+                if (SqlCon.State == ConnectionState.Closed)
+                { SqlCon.Open(); }
+                String[] tenbien = { "bachoc", "tenkh", "tenng", "tenct", "namvao", "malophoc", "tenmon" };
+               
+                List<string> bienluu = new List<string>();
+
+
+                //for (int i = 0; i < comboBox.Length; i++)
+                //{
+                //    comboBox[i].SelectedIndexChanged += (o, e) =>
+                //    {
+                //        string opt = comboBox[i].SelectedItem.ToString();
+                //        suffer += String.Format(" AND %s = '%s'", tenbien[i], opt);
+                //    }
+                //}
+
+                SqlCommand[] cmds = new SqlCommand[tenbien.Length]; for (int i = 0; i < cmds.Length; i++) cmds[i] = new SqlCommand();
+                SqlDataReader[] drs = new SqlDataReader[tenbien.Length];
+                for (int i = 0; i < tenbien.Length; i++)
+                {
+                    String cmd = suffer;
+                    comboBox[i].SelectedIndexChanged += (o, e) =>
+                        {
+                            for (int j = i + 1; j < tenbien.Length; j++)
+                            {
+                                throw new ArithmeticException("1");
+                                throw new ArithmeticException(suffer.Replace("@X", tenbien[j]).Replace(String.Format("%s=%s", i, i), opt));
+                                cmds[j].CommandText = suffer.Replace("@X", tenbien[j]).Replace(String.Format("%s=%s", i, i), opt);
+                                
+                                drs[j] = cmds[j].ExecuteReader();
+                                while (drs[j].Read())
+                                {
+                                    //bienluu.Add(drs[i].ToString());
+
+                                    if (j != 4 && !comboBox[j].Items.Contains(drs[j].GetString(0)))
+                                    {
+                                        comboBox[j].Items.Add(drs[j].GetString(0));
+                                    }
+                                    else if (j == 4 && !comboBox[j].Items.Contains((drs[j].GetInt32(0))))
+                                    {
+                                        comboBox[j].Items.Add(drs[j].GetInt32(0));
+                                    }
+                                    //drs[j].Close();
+                                }
+
+                            }
+                        };
+                    cmds[i].CommandText = suffer.Replace("@X", tenbien[i]);
+                    cmds[i].Connection = SqlCon;
+                    drs[i] = cmds[i].ExecuteReader();
+                    while (drs[i].Read())
+
+                        //bienluu.Add(drs[i].ToString());
+
+                        if (i!=4 && !comboBox[i].Items.Contains(drs[i].GetString(0)))
+                        {
+                            comboBox[i].Items.Add(drs[i].GetString(0));
+                        }
+                        else if (i == 4 && !comboBox[i].Items.Contains((drs[i].GetInt32(0))))
+                    {
+                        comboBox[i].Items.Add(drs[i].GetInt32(0));
+                    }
+
+                    drs[i].Close();
+                }
+                
+
+                //comboBox[0].SelectedIndexChanged += (o, e) =>
+                //{
+
+                //}
+
+
+
+
+
+
+
+            }
+
+
+        }
+    }
+}
+
+    
+
+
+
+
